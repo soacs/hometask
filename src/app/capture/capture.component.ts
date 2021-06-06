e44f117df612b599b068c068e1a32482df8ae3b9
buonincontris <buonincontris@foliofinancial.com>
2021-06-06 11:12:58 -0400

    capture working

--- src/app/capture/capture.component.ts
+++ src/app/capture/capture.component.ts
@@ -1,18 +1,46 @@
-
 import { Component, OnInit } from "@angular/core";
+import * as camera from "@nativescript/camera";
+import { Image } from "@nativescript/core";
 
 @Component({
     selector: "capture",
-    templateUrl: "./capture.component.html",
+    templateUrl: "capture.component.html",
     styleUrls: ["./capture.component.scss"]
 })
 export class CaptureComponent implements OnInit {
 
     constructor() {
-        console.log('ENTER: CaptureComponent constructor');
+        console.log('CaptureComponent constructor');
     }
 
-    // tslint:disable-next-line:no-empty
-    ngOnInit(): void {
+    ngOnInit() {
+        console.log('CaptureComponent ngOnInit');
+    }
+
+    captureIt($event) {
+        alert('capturing: ');
+        console.log('ENTER: CaptureComponent capture()');
+        const options = {
+            width: 300,
+            height: 300,
+            keepAspectRatio: false,
+            saveToGallery: true
+        };
+        camera.requestPermissions().then(
+            function success() {
+
+                camera.takePicture(options)
+                    .then((imageAsset) => {
+                        console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+                        console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+                        console.log("Photo saved in Photos/Gallery for Android or in Camera Roll for iOS");
+                    }).catch((err) => {
+                    console.log("Error -> " + err.message);
+                });
+            },
+            function failure() {
+                console.log("Error -> " + 'Permission denied');
+            }
+        );
     }
 }
